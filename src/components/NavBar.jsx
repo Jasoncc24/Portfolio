b6d82b2c2f4aeedf451fa9bb154be3c8acfb31b8
Jasoncc24 <77653071+Jasoncc24@users.noreply.github.com>
2022-12-09 18:46:15 -0500

    change to div

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -31,7 +31,7 @@ const NavBar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed nav">
       <div>
-        <h1 className="text-5xl font-signature ml-2"><a className="link-underline link-underline-black">Jason C</a></h1>
+        <h1 className="text-5xl font-signature ml-2"><div className="link-underline link-underline-black">Jason C</div></h1>
       </div>
 
       <ul className="hidden md:flex">
